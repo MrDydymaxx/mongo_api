6c0cd8bfe9f99219d6908d8c06f254472639e7e1
MrDydymaxx <mrdydymmaxx@gmail.com>
2022-04-12 14:55:04 +0200

    base api

---
+++ app/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    dev: {
+        express: {
+            port: 3000
+        },
+        mongodb:  {
+            host:'mongodb://localhost:2717/tp'
+        }
+    },
+    prod: {
+        express: {
+            port: 3000
+        },
+        mongodb:  {
+            host:'mongodb://localhost:2717/tp'
+        }
+    }
+}
